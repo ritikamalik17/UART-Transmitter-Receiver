caea4023e8d067eeb1e1e4390fa6e00705625151
ritikamalik17 <ritikamalik1706@gmail.com>
2026-07-16 10:30:08 +0530

    Update baud_generator.v

--- rtl/baud_generator.v
+++ rtl/baud_generator.v
@@ -1,1 +1,33 @@
+module baud_generator #(
+    parameter DIVISOR = 5208
+)(
+    input clk,
+    input reset,
+    output reg tick
+);
 
+reg [12:0] counter;
+
+always @(posedge clk)
+begin
+    if (reset)
+    begin
+        counter <= 13'd0;
+        tick <= 1'b0;
+    end
+    else
+    begin
+        if (counter == DIVISOR - 1)
+        begin
+            counter <= 13'd0;
+            tick <= 1'b1;
+        end
+        else
+        begin
+            counter <= counter + 1'b1;
+            tick <= 1'b0;
+        end
+    end
+end
+
+endmodule
